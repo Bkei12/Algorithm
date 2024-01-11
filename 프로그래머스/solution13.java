6329744e569c5ee9b4b09309056209f45f4720fe
Ko Bkei <101390721+Bkei12@users.noreply.github.com>
2024-01-11 13:57:38 +0900

    Create solution13.java

---
+++ 프로그래머스/solution13.java
@@ -0,0 +1,22 @@
+class Solution13 {
+    public int[] solution(int[] numbers, String direction) {
+        int n = numbers.length;
+        int[] result = new int[n];
+
+        if (direction.equals("right")) {
+            // 오른쪽으로 이동
+            for (int i = 0; i < n - 1; i++) {
+                result[i + 1] = numbers[i];
+            }
+            result[0] = numbers[n - 1];
+        } else if (direction.equals("left")) {
+            // 왼쪽으로 이동
+            for (int i = 1; i < n; i++) {
+                result[i - 1] = numbers[i];
+            }
+            result[n - 1] = numbers[0];
+        }
+
+        return result;
+    }
+}
