e21b36318632108ab0834e1ae47c52fd379be6c5
Byeong-Kuk Ko <101390721+Bkei12@users.noreply.github.com>
2024-02-08 15:33:11 +0900

    Create solution29.java

---
+++ 프로그래머스/solution29.java
@@ -0,0 +1,15 @@
+// 세로 읽기
+
+class Solution {
+    public String solution(String my_string, int m, int c) {
+        StringBuilder answer = new StringBuilder();
+
+        int startIndex = c - 1;
+        while (startIndex < my_string.length()) {
+            answer.append(my_string.charAt(startIndex));
+            startIndex += m;
+        }
+
+        return answer.toString();
+    }
+}
