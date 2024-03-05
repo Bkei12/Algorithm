dc0622d86b766050c0b4c0c3751a9092ee049df2
Byeong-Kuk Ko <101390721+Bkei12@users.noreply.github.com>
2024-03-05 20:14:15 +0900

    Create solution34.java

---
+++ 프로그래머스/solution34.java
@@ -0,0 +1,16 @@
+// 문자열이 몇 번 등장하는지 세기
+
+class Solution {
+    public int solution(String myString, String pat) {
+        int answer = 0;
+        int msLength = myString.length();
+        int pLength = pat.length();
+        StringBuilder sb = new StringBuilder(myString);
+        for(int i = 0; i <= msLength-pLength; i++) {
+            if((sb.substring(i, i+pLength).equals(pat))) {
+                answer++;
+            }
+        }
+        return answer;
+    }
+}
